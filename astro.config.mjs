30c1965ae070dc22ca156d773687591b46e3a329
Tsuki Akiba <erase2004@gmail.com>
2025-09-26 15:16:28 +0800

    feat: add favicon and webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,40 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import favicons from 'astro-favicons';
+import { title } from './src/config';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	site: "https://museum.acgn-stock.com",
-	image: {
-		responsiveStyles: true
-	}
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  site: "https://museum.acgn-stock.com",
+
+  image: {
+    responsiveStyles: true
+  },
+  integrations: [favicons({
+		input: {
+			favicons: [
+				"src/assets/icons/logo.png"
+			]
+		},
+		name: title,
+		short_name: 'Museum Landmark',
+		icons: {
+			favicons: true,
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        windows: true,
+				yandex: false
+		},
+		output: {
+			images: true,
+			files: true,
+			html: true,
+    },
+	})]
 });
